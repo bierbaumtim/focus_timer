02a3a5e7e846a14fa727092fdfaec497165f4a51
tb2761 <tb2706@outlook.de>
2020-02-23 12:14:33 +0100

    implement digital clock

--- lib/widgets/time/countdown_time.dart
+++ lib/widgets/time/countdown_time.dart
@@ -1,3 +1,4 @@
+import 'package:flip_panel/flip_panel.dart';
 import 'package:flutter/material.dart';
 
 import 'package:auto_size_text/auto_size_text.dart';
@@ -9,33 +10,81 @@ import '../../utils/time_utils.dart';
 
 class CountdownTime extends StatelessWidget {
   final bool isSmall;
+  final bool useDigitalClock;
 
   const CountdownTime({
     Key key,
     this.isSmall = false,
+    this.useDigitalClock = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     final currentSessionModel = Injector.get<CurrentSessionModel>();
 
     return StateBuilder<SessionsModel>(
       models: [
         currentSessionModel,
       ],
-      builder: (context, _) => Align(
-        alignment: Alignment.center,
-        child: AutoSizeText(
-          timeToString(currentSessionModel.currentDuration),
-          maxLines: 1,
-          style: theme.textTheme.title.copyWith(
-            fontSize: 110,
-            shadows: [],
-          ),
-          maxFontSize: isSmall ? 20 : double.infinity,
-        ),
+      builder: (context, _) => Center(
+        child: isSmall
+            ? _SimpleTime(
+                duration: currentSessionModel.currentDuration,
+                isSmall: isSmall,
+              )
+            : useDigitalClock
+                ? _DigitalTime(
+                    duration: currentSessionModel.currentDuration,
+                  )
+                : _SimpleTime(
+                    duration: currentSessionModel.currentDuration,
+                    isSmall: isSmall,
+                  ),
       ),
+    );
+  }
+}
+
+class _DigitalTime extends StatelessWidget {
+  final int duration;
+
+  const _DigitalTime({Key key, @required this.duration}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return SizedBox(
+      height: 64,
+      child: FlipClock.countdown(
+        duration: Duration(seconds: duration),
+        digitColor: theme.textTheme.headline6.color,
+        backgroundColor: theme.canvasColor,
+        digitSize: theme.textTheme.headline6.fontSize,
+      ),
+    );
+  }
+}
+
+class _SimpleTime extends StatelessWidget {
+  final int duration;
+  final bool isSmall;
+
+  const _SimpleTime({
+    Key key,
+    @required this.duration,
+    this.isSmall = false,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AutoSizeText(
+      timeToString(duration),
+      maxLines: 1,
+      style: Theme.of(context).textTheme.headline6.copyWith(
+        fontSize: 110,
+        shadows: [],
+      ),
+      maxFontSize: isSmall ? 20 : double.infinity,
     );
   }
 }
